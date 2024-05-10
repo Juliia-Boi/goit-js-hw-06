2075d8e31f9bbd9e81c47e3594ce044efebfbf7c
Juliia-Boi <160080389+Juliia-Boi@users.noreply.github.com>
2024-05-10 15:51:19 +0300

    save

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,15 @@
-function getElementWidth(content, padding, border) {
-    const totalWidth = parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-    return totalWidth;
+function filterArray(numbers, value) {
+  const result = [];
+  for (let i = 0; numbers.length > i; i += 1) {
+    if (numbers[i] > value) {
+      result.push(numbers[i]);
+    }
+  }
+  return result;
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
